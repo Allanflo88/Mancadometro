d532f584d85f2e8042806706cfc7347dc4cdf95d
Allan F. de Jesus <ajesus@dcc.local>
2018-12-19 16:32:20 -0200

    initial project

---
+++ mancadometro/src/providers/storage/storage.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import moment from 'moment';
+import Mancada from '../../app/models/mancada';
+
+
+/*
+  Generated class for the StorageProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class StorageProvider {
+
+  constructor(private storage: Storage) {
+  }
+
+  get(): Promise<any>{
+    return new Promise((resolve, reject)=>{
+      console.log("mancadas_" + this.convertDate(moment().toString()))
+      this.storage.get("mancadas_" + this.convertDate(moment().toString())).then((res)=>{
+        resolve(res);
+      }).catch((err)=>{
+        reject(err);
+      })
+    })
+  }
+
+  set(mancada:Mancada){
+    mancada.date = this.convertDate(moment().toString())
+    this.storage.set("mancadas_" + mancada.date, mancada);
+  }
+
+  convertDate(date:string): String{
+    return moment(date).format("DD-MM-YYYY");
+  }
+
+}
